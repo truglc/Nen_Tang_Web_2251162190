6bcf5107723ddcf28047d0b290b7663cbeb5a311
truglc <trunglc9388@gmail.com>
2025-08-03 21:45:08 +0700

    bai1

---
+++ de1/data.js
@@ -0,0 +1,32 @@
+const employees = [
+  {
+    name: "Thomas Hardy",
+    email: "thomashardy@mail.com",
+    address: "89 Chiaroscuro Rd, Portland, USA",
+    phone: "(171) 555-2222"
+  },
+  {
+    name: "Dominique Perrier",
+    email: "dominiqueperrier@mail.com",
+    address: "Obere Str. 57, Berlin, Germany",
+    phone: "(313) 555-5735"
+  },
+  {
+    name: "Maria Anders",
+    email: "mariaanders@mail.com",
+    address: "25, rue Lauriston, Paris, France",
+    phone: "(503) 555-9931"
+  },
+  {
+    name: "Fran Wilson",
+    email: "franwilson@mail.com",
+    address: "C/ Araquil, 67, Madrid, Spain",
+    phone: "(204) 619-5731"
+  },
+  {
+    name: "Martin Blank",
+    email: "martinblank@mail.com",
+    address: "Via Monte Bianco 34, Turin, Italy",
+    phone: "(480) 631-2097"
+  }
+];
