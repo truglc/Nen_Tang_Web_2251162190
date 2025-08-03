31944507d8848f0d3702ac436af73f0df16fd95b
truglc <trunglc9388@gmail.com>
2025-08-03 21:16:18 +0700

    de3

---
+++ de3/data.js
@@ -0,0 +1,26 @@
+const initialData = [
+  {
+    "firstName": "An",
+    "lastName": "Nguyễn Văn",
+    "address": "Hà Nội",
+    "activity": "✔"
+  },
+  {
+    "firstName": "Bình",
+    "lastName": "Trần Thị",
+    "address": "TP.HCM",
+    "activity": "✘"
+  },
+  {
+    "firstName": "Lan",
+    "lastName": "Phạm Mai",
+    "address": "Đà Nẵng",
+    "activity": "✔"
+  },
+  {
+    "firstName": "Minh",
+    "lastName": "Lê Văn",
+    "address": "Cần Thơ",
+    "activity": "✘"
+  }
+];
